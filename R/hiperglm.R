51428e8853aba9ebc697bfa86cd7da6c5a9d6eae
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-09 21:58:46 -0500

    Added hiperglm package

---
+++ R/hiperglm.R
@@ -0,0 +1,9 @@
+#' @export
+hiper_glm <- function(design, outcome) {
+  warning("hiperglm is yet to be implemented")
+  # TODO: maximize likelihood
+  hglm_out <- list()
+  class(hglm_out) <- "hglm"
+  return(hglm_out)
+}
+
---
+++ R/model_output.R
@@ -0,0 +1,14 @@
+#' @export
+print.hglm <- function(hglm_out) {
+  cat("hiperglm output\n")
+}
+
+#' @export
+coef.hglm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+#' @export
+vcov.hglm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
