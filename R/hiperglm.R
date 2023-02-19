0e4a99ead64e1c88f453ed1f89d92130d9c701af
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-19 15:33:24 -0500

    Add if else to hiper_glm function to enable users to do either pseudo-inverse or BFGS

--- R/hiperglm.R
+++ R/hiperglm.R
@@ -1,6 +1,16 @@
 #' @export
-hiper_glm <- function(design, outcome) {
-  warning("hiperglm is yet to be implemented")
+hiper_glm <- function(design, outcome, model = "linear", option = list()) {
+  supported_model <- c("linear", "logit")
+  if(!(model %in% supported_model)) {
+    stop(sprintf("The model %s is not supported", model))
+  }
+
+  if (length(option) == 0) {
+
+  } else if (option == "BFGS") {
+
+  }
+
   # TODO: maximize likelihood
   hglm_out <- list()
   class(hglm_out) <- "hglm"
