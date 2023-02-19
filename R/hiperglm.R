54321da28ece6c56172a9be84303fa8869fa3ccc
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-19 15:36:49 -0500

    add comments 'MLE finder via pseudo-inverse' and 'MLE finder via BFGS'

--- R/hiperglm.R
+++ R/hiperglm.R
@@ -4,10 +4,12 @@ hiper_glm <- function(design, outcome, model = "linear", option = list()) {
   if(!(model %in% supported_model)) {
     stop(sprintf("The model %s is not supported", model))
   }
-
+  # MLE finder via pseudo-inverse
   if (length(option) == 0) {
 
-  } else if (option == "BFGS") {
+  }
+  # MLE finder via BFGS
+  else if (option == "BFGS") {
 
   }
 
