581b75afa629fa81c32d76a1231e6ad098337955
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-19 11:33:18 -0500

    write a test to compare pseudo-inverse and BFGS methods for MLE finder

---
+++ tests/testthat/test-mle-finder.R
@@ -0,0 +1,13 @@
+testthat::test_that(
+  "comparison between MLE estimated via pseudo-inverse and via BFGS",
+  {
+    n_obs <- 32; n_pred <- 4
+    data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+    design <- data$design; outcome <- data$outcome
+    pseudo_inverse_out <- hiperglm::hiper_glm(design, outcome, model = "linear")
+    bfgs_out <- hiperglm::hiper_glm(design, outcome, model = "linear", option = list(mle_solver = 'BFGS'))
+    expect_true(are_all_close(
+      coef(pseudo_inverse_out), coef(bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
+    ))
+  }
+)
