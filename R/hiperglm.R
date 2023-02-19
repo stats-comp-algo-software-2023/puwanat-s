4349aea079c85d1c4f51bda8c34100cf442408dc
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-19 15:40:57 -0500

    calculate single value decomposition of the design matrix

--- R/hiperglm.R
+++ R/hiperglm.R
@@ -6,7 +6,7 @@ hiper_glm <- function(design, outcome, model = "linear", option = list()) {
   }
   # MLE finder via pseudo-inverse
   if (length(option) == 0) {
-
+    design.svd <- svd(design)
   }
   # MLE finder via BFGS
   else if (option == "BFGS") {
