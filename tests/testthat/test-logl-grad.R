4395ad8a14e527111036640357f24d5b77fa1b15
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-20 15:09:27 -0500

    add 'test-logl-grad.R' in testthat folder for a comprison between the analytical and numerical method

---
+++ tests/testthat/test-logl-grad.R
@@ -0,0 +1,20 @@
+testthat::test_that(
+  "compare numerical (centered difference method) and analytical methods to calculate a gradient of log-likelihood function",
+  {
+    simulated_data <- simulate_data(n_obs = 32, n_pred = 4)
+    X <- simulated_data$design
+    y <- simulated_data$outcome
+    coef_true <- simulated_data$coef_true
+
+    analytical_grad <- logl_grad(coef_true, y, X)
+    numerical_grad <- approx_grad(function(x) logl(coef_true, y, X), X)
+
+    expect_true(are_all_close(
+      analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
+    ))
+  }
+)
+
+
+
+
