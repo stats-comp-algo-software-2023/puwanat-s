63fe882cd5b3587bcf9b271d243b47e2e39d759d
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-20 14:06:56 -0500

    create function 'logl' to calculate log-likelihood of a linear model

--- R/hiperglm.R
+++ R/hiperglm.R
@@ -21,3 +21,11 @@ hiper_glm <- function(design, outcome, model = "linear", option = list()) {
   return(hglm_out)
 }
 
+#' @export
+logl <- function(coef, noise_var = 1, y, X) {
+  m <- nrow(X)
+  n <- ncol(X)
+  e <- y - (X %*% coef)
+  logl_out <- -.5*m*log(2*pi)-.5*m*log(noise_var)-((t(e) %*% e)/(2*noise_var))
+  return(-logl_out)
+}
