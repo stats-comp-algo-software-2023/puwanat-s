b0aa82a8f63deac7119046e59ce7fe31053da962
Puwanat Sangkhapreecha <a.puwanat@gmail.com>
2023-02-25 17:05:52 -0500

    remove the second and the third tests in test_are_all_close.R

--- tests/testthat/test_are_all_close.R
+++ tests/testthat/test_are_all_close.R
@@ -2,8 +2,6 @@ testthat::test_that(
   "the relative error is below rel_tol and abs_tol",
   {
     expect_true(are_all_close(1, 1, abs_tol = 1e-2, rel_tol = 1e-2))
-    expect_false(are_all_close(1, 2, abs_tol = 2, rel_tol = 1e-2))
-    expect_false(are_all_close(1, 2, abs_tol = 1e-2, rel_tol = 2))
   }
 )
 
